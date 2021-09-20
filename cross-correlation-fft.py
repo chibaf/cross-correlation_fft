c9b0c9a94059808b81ef6143489ad52cb18b6748
chibaf <chibaf@mac.com>
2021-09-21 02:41:12 +0900

    Add files via upload

--- cross-correlation-fft.py
+++ cross-correlation-fft.py
@@ -45,15 +45,3 @@ plt.plot(x,corrf)
 plt.show()
 
 exit()
-#output the result to file sys.argv[2]
-#f = open(sys.argv[2], 'w') 
-#st=str(corr[0])
-#for i in range(1,len(corr)):
-#  if i!=(len(corr)-1):
-#    st=st+","+str(corr[i])
-#  else:
-#    st=st+str(corr[i])+"\n"
-#f.write(st)
-#f.write(str(ix))
-#f.close()
-
