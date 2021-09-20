e3213750010c21085ce2d284b4f943c3f2914eeb
chibaf <chibaf@mac.com>
2021-09-21 02:50:56 +0900

    Add files via upload

--- cross-correlation-fft.py
+++ cross-correlation-fft.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
-def find_index(c):
+def find_index(c):  # find index of maximum value
   mc=np.amax(c)
   for i in range(len(c)):
     if c[i]==mc:
@@ -14,14 +14,15 @@ m=np.loadtxt(sys.argv[1],delimiter=',')  # convert a csv file to a matrix from f
 
 mt=m.T #transpose matrix
 
-#computing cross-correlation
+#computing cross-correlation by shift
 v1=mt[1][10000:15000];v2=mt[2][10000:15000]
-c=1.0/(np.linalg.norm(v1)*np.linalg.norm(v2))   # added on 16.Sep.2021
+c=1.0/(np.linalg.norm(v1)*np.linalg.norm(v2))
 corr=np.empty(0)   #make nd.array of length zero
-corr=np.append(corr,np.dot(v1,v2)*c)  # the first element of corr, modified on 16.Sep.2021
+corr=np.append(corr,np.dot(v1,v2)*c)
 for i in range(1,len(v1)):
   v2=np.roll(v2,1)  # shift 1 to the right
-  corr=np.append(corr,np.dot(v1,v2)*c)   # cross correlation, modified on 16.Sep.2021
+  corr=np.append(corr,np.dot(v1,v2)*c)
+#find max
 print(np.amax(corr))
 print(find_index(corr))
 
@@ -30,6 +31,7 @@ x=range(len(corr)) #plot array
 plt.plot(x,corr)
 plt.show()
 
+#computing cross-correlation by fft
 v1=mt[1][10000:15000];v2=mt[2][10000:15000]
 c=1.0/(np.linalg.norm(v1)*np.linalg.norm(v2)) 
 fft_len=13
@@ -37,6 +39,7 @@ f1=np.fft.fft(v1)
 f2=np.fft.fft(v2)
 ff=f1*f2
 corrf=np.real(np.fft.ifft(ff))*c
+#find max
 print(np.amax(corrf))
 print(find_index(corrf))
 
